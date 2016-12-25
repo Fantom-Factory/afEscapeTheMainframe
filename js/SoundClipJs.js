eeabf93f3e307027f181a7dba68b6c4b204958c2
Steve Eynon <3326741+SlimerDude@users.noreply.github.com>
2016-12-25 21:49:44 +0000

    Added sound.stop() in JS and fixed IE bug

--- js/SoundClipJs.js
+++ js/SoundClipJs.js
@@ -9,9 +9,9 @@ fan.afFannyTheFantom.SoundClipJs.prototype.$ctor = function() {
 			if (!context.createGain)
 				context.createGain = context.createGainNode;
 		} catch(e) {
-			console.warn(e);
+			// console.warn(e);
 			fan.afFannyTheFantom.SoundClipJs.context = null;
-			console.warn("No Audio Context === No Sound Effects");
+			console.warn("No Audio Context === No Sound Effects ... :(");
 		}
 }
 
@@ -19,20 +19,24 @@ fan.afFannyTheFantom.SoundClipJs.prototype.$typeof = function() {
 	return fan.afFannyTheFantom.SoundClipJs.$type;
 }
 
-fan.afFannyTheFantom.SoundClipJs.prototype.m_buffer = null;
+fan.afFannyTheFantom.SoundClipJs.prototype.m_buffer   = null;
+fan.afFannyTheFantom.SoundClipJs.prototype.m_source   = null;
+fan.afFannyTheFantom.SoundClipJs.prototype.m_gainNode = null;
+
 fan.afFannyTheFantom.SoundClipJs.make = function(soundUri) {
 	var soundJs = new fan.afFannyTheFantom.SoundClipJs();
 
 	if (soundUri.scheme() != "fan")
 		throw fan.sys.ArgErr.make("Uri scheme must be 'fan': " + soundUri.toStr());
 	var soundUrl = fan.sys.UriPodBase + soundUri.host() + soundUri.pathStr();
+	soundJs.m_url = soundUrl;
 
 	var context = fan.afFannyTheFantom.SoundClipJs.context;
 	if (context == null)
 		return soundJs;
 
-	soundJs.m_gainNode	= context.createGain();
-	soundJs.m_url		= soundUrl;
+	// create now so we can immediately set the volume
+	soundJs.m_gainNode = context.createGain();
 
 	var request = new XMLHttpRequest();
 	request.open("GET", soundUrl, true);
@@ -48,23 +52,28 @@ fan.afFannyTheFantom.SoundClipJs.make = function(soundUri) {
 }
 
 fan.afFannyTheFantom.SoundClipJs.prototype.loaded = function() {
-	return this.m_buffer != null
+	// return 'true' if no sounds so the start screen thinks all the clips have loaded.
+	return this.m_buffer != null || fan.afFannyTheFantom.SoundClipJs.context == null;
 }
 
 fan.afFannyTheFantom.SoundClipJs.prototype.play = function() {
 	var context = fan.afFannyTheFantom.SoundClipJs.context;
 	if (context == null)
 		return;
+	if (this.m_buffer == null)
+		return;
 
-	var source = context.createBufferSource();
-	source.buffer = this.m_buffer;
-	source.connect(this.m_gainNode);
+	this.m_source = context.createBufferSource();
+	this.m_source.buffer = this.m_buffer;
+	this.m_source.connect(this.m_gainNode);
 	this.m_gainNode.connect(context.destination);
-	source.start(0);
+	this.m_source.start();
 }
 
 fan.afFannyTheFantom.SoundClipJs.prototype.stop = function() {
-	console.info("Stop!")
+	if (this.m_source == null)
+		return;
+	this.m_source.stop();
 }
 
 fan.afFannyTheFantom.SoundClipJs.prototype.m_volume = 1;
@@ -75,6 +84,6 @@ fan.afFannyTheFantom.SoundClipJs.prototype.volume$ = function(volume) {
 		throw fan.sys.ArgErr.make("Invalid volume: " + volVal);
 	this.m_volume = volVal;
 
-	if (this.m_gainNode)
+	if (this.m_gainNode != null)
 		this.m_gainNode.gain.value = volVal;
 }
